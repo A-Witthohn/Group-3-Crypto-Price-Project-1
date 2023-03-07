5ff7629c7c1dc192fab612843c2a704ee4c1fb43
dspnc <dspence.hill@gmail.com>
2023-03-06 19:17:00 -0500

    event listeners to set chosen coin and run lookup

--- javascript.js
+++ javascript.js
@@ -5,3 +5,30 @@ const dropDown=document.querySelector('.dropdown');
 dropDownButton.addEventListener('click', () =>{
     dropDown.classList.toggle('is-active');
 });
+
+var btcEl = document.getElementById("btc")
+var ethEl = document.getElementById("eth")
+var ltcEl = document.getElementById("ltc")
+var xrpEl = document.getElementById("xrp")
+var chosenCoin;
+
+btcEl.addEventListener("click", function(){
+    chosenCoin = "btc";
+    cryptoLookup(chosenCoin);
+})
+ethEl.addEventListener("click", function (){
+    chosenCoin = "eth";
+    cryptoLookup(chosenCoin);
+})
+ltcEl.addEventListener("click", function (){
+    chosenCoin = "ltc";
+    cryptoLookup(chosenCoin);
+})
+xrpEl.addEventListener("click", function (){
+    chosenCoin = "xrp";
+    cryptoLookup(chosenCoin);
+} )
+
+var cryptoLookup = function (crypto) {
+    console.log("chosen coin is " + crypto)
+}
