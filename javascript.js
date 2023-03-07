29ad110cf422d51fcf8ff96525b274fa9f2c6161
James A Cone <jacone626@gmail.com>
2023-03-06 19:53:17 -0500

    added API key for website urls

--- javascript.js
+++ javascript.js
@@ -6,9 +6,39 @@ dropDownButton.addEventListener('click', () =>{
     dropDown.classList.toggle('is-active');
 });
 
+var btcEl = document.getElementById("btc")
+var ethEl = document.getElementById("eth")
+var ltcEl = document.getElementById("ltc")
+var xrpEl = document.getElementById("xrp")
+var chosenCoin;
+
+btcEl.addEventListener("click", function(){
+    chosenCoin = "bitcoin";
+    // cryptoLookup(chosenCoin);
+    cryptoUrl(chosenCoin);
+})
+ethEl.addEventListener("click", function (){
+    chosenCoin = "ethereum";
+    // cryptoLookup(chosenCoin);
+    cryptoUrl(chosenCoin);
+})
+ltcEl.addEventListener("click", function (){
+    chosenCoin = "litecoin";
+    // cryptoLookup(chosenCoin);
+    cryptoUrl(chosenCoin);
+})
+xrpEl.addEventListener("click", function (){
+    chosenCoin = "ripple";
+    // cryptoLookup(chosenCoin);
+    cryptoUrl(chosenCoin);
+} )
+
+var cryptoUrl = function (crypto) {
+    console.log("chosen coin is " + crypto)
+
+
 //Adding API key and information for coins URLs
-function testApi () {}
-var cryptoUrl = "https://api.coinstats.app/public/v1/coins?skip=0&limit=10&currency=USD";
+var cryptoUrl = "https://api.coinstats.app/public/v1/coins/" + crypto + "?skip=0&limit=10&currency=USD";
 
 fetch(cryptoUrl)
 .then(function (response) {
@@ -17,3 +47,4 @@ fetch(cryptoUrl)
 .then(function (data) {
   console.log(data)
   })
+}
