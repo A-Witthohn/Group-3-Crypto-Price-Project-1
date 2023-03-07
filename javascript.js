3ae7b7a5baed65ce68f43d28eda4d2689d9c2a7f
James A Cone <jacone626@gmail.com>
2023-03-06 19:20:22 -0500

    started working on API key

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ const dropDown=document.querySelector('.dropdown');
 dropDownButton.addEventListener('click', () =>{
     dropDown.classList.toggle('is-active');
 });
+
+//Adding API key and information for coins URLs
+function testApi () {}
+var cryptoUrl = "https://api.coinstats.app/public/v1/coins?skip=0&limit=10&currency=USD";
+
+fetch(cryptoUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data)
+  })
